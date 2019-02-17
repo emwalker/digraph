61f07365c5658e590398564fe7dccc55f6a8f544
Eric Walker <eric.walker@gmail.com>
2019-02-17 14:00:15 -0700

    Try to better accomodate iphones

--- src/components/ui/RightColumn/index.jsx
+++ src/components/ui/RightColumn/index.jsx
@@ -6,7 +6,7 @@ type Props = {
 }
 
 const RightColumn = ({ children }: Props) => (
-  <div className="col-lg-4 col-md-6 col-12 float-right pb-3">
+  <div className="col-lg-4 col-12 float-right pb-3">
     { children }
   </div>
 )
