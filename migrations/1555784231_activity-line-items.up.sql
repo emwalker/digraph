210b54503a68f717f6027daf9ca226decd53734f
Eric Walker <eric.walker@gmail.com>
2019-04-20 23:24:54 -0600

    Hotfix: use new uuid gen function

--- migrations/1555784231_activity-line-items.up.sql
+++ migrations/1555784231_activity-line-items.up.sql
@@ -3,7 +3,7 @@ alter table user_links alter column created_at set not null;
 create type topic_action as enum ('topic_added', 'topic_removed');
 
 create table user_link_topics (
-  id uuid primary key default uuid_generate_v1mc(),
+  id uuid primary key default gen_random_uuid(),
   user_link_id uuid not null references user_links(id) on delete cascade,
   action topic_action not null,
   topic_id uuid not null references topics(id) on delete cascade
