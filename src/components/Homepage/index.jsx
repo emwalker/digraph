518d583487df40864f775771da8c339b5dc076e5
Eric Walker <eric.walker@gmail.com>
2019-09-08 12:17:35 -0600

    Further tweaks to the landing page

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -32,15 +32,17 @@ const Homepage = ({ view, router }: Props) => {
     <div
       className={classNames(styles.container, 'container-lg f4 px-3 px-md-6 px-lg-0')}
     >
-      <h2 className="mb-2">
-        Digraph
-      </h2>
+      <div className={styles.hero}>
+        <h1 className="mb-4">
+          Digraph
+        </h1>
 
-      <p>
-        Save links in a mind map-like network of topics. Keep track of everything
-        you&apos;ve read or might want to read in the future. Gain control over your
-        reading and turn the deluge of information into knowledge.
-      </p>
+        <p>
+          Save links in a mind map-like network of topics. Keep track of everything
+          you&apos;ve read or might want to read in the future. Gain control over your
+          reading and turn the deluge of information into knowledge.
+        </p>
+      </div>
 
       <h4>Recent updates</h4>
       <div>
