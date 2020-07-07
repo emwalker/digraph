9b57d5beda8afe73941d6df61101214f2b730a57
Eric Walker <eric.walker@gmail.com>
2020-07-06 21:30:24 -0600

    Allow the user to select whether to search within the present topic or within all topics

--- src/components/ui/SearchBox/index.jsx
+++ src/components/ui/SearchBox/index.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import classNames from 'classnames'
 
 import { everythingTopicPath } from 'components/constants'
@@ -12,8 +12,8 @@ type Props = {
   router: Router,
 }
 
-const pathnameFor = (pathname: string) => (
-  pathname === '/' ? everythingTopicPath : pathname
+const pathnameFor = (pathname: string, selectedScope: string) => (
+  selectedScope === 'Everything' ? everythingTopicPath : pathname
 )
 
 const onFormSubmit = (event: SyntheticKeyboardEvent<HTMLButtonElement>) => {
@@ -22,33 +22,37 @@ const onFormSubmit = (event: SyntheticKeyboardEvent<HTMLButtonElement>) => {
 
 const SearchBox = ({ className, router, location }: Props) => {
   const { pathname } = location
-
-  const searchString = location.search
-    ? location.query.q
-    : ''
-
-  const scopeLabel = pathname === '/' ? 'Everything' : 'This topic'
+  const searchString = location.search ? location.query.q : ''
+  const [selectedScope, setSelectedScope] = useState('Everything')
 
   const onKeyPress = useCallback((event: SyntheticKeyboardEvent<HTMLButtonElement>) => {
     if (event.key === 'Enter') {
       const { value } = (event.target: window.HTMLInputElement)
-      const newPathname = pathnameFor(pathname)
+      const searchPathname = pathnameFor(pathname, selectedScope)
 
       if (value === '') {
-        router.push({ pathname: newPathname })
+        router.push({ pathname: searchPathname })
         return
       }
 
-      router.push({ pathname: newPathname, query: { q: value } })
+      router.push({ pathname: searchPathname, query: { q: value } })
     }
-  }, [router, pathname])
+  }, [router, pathname, selectedScope])
+
+  const onSelectChange = useCallback((event: SyntheticKeyboardEvent<HTMLButtonElement>) => {
+    const { value } = (event.target: window.HTMLInputElement)
+    setSelectedScope(value)
+  })
 
   const actualClassName = classNames(styles.searchBox, 'input-group', className)
 
   return (
     <form className={actualClassName} onSubmit={onFormSubmit}>
       <span className="input-group-button">
-        <button className="btn" type="button">{scopeLabel}</button>
+        <select onChange={onSelectChange} value={selectedScope} className="btn">
+          <option>This topic</option>
+          <option>Everything</option>
+        </select>
       </span>
       <input
         aria-label="Search"
