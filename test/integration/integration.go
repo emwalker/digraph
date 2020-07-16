a536b3ac40cc8f87672cad22982ac42d714be575
Eric Walker <eric.walker@gmail.com>
2020-07-15 21:16:29 -0600

    Save tests from reverted bugfix

--- test/integration/integration.go
+++ test/integration/integration.go
@@ -137,7 +137,7 @@ func NewTestDB() (*sql.DB, error) {
 func (m Mutator) MakeTopic(options TopicOptions) *models.TopicValue {
 	ctx := context.Background()
 	c := services.Connection{Exec: m.DB, Actor: m.Actor}
-	result, err := c.UpsertTopic(ctx, TestRepository, options.Name, nil, []string{})
+	result, err := c.UpsertTopic(ctx, TestRepository, options.Name, nil, options.ParentTopicIds)
 	Must(err)
 	return &models.TopicValue{result.Topic, true, TestView}
 }
