0f48dbffce88ab9506b3315d446473ab42a28289
Eric Walker <eric.walker@gmail.com>
2022-06-25 08:14:41 -0600

    Fix 'Everything' link

--- javascript/src/components/navigation.ts
+++ javascript/src/components/navigation.ts
@@ -1,7 +1,7 @@
 /* eslint import/prefer-default-export: 0 */
 
 export const toEverything = {
-  pathname: '/wiki/topics/df63295e-ee02-11e8-9e36-17d56b662bc8',
+  pathname: '/wiki/df63295e-ee02-11e8-9e36-17d56b662bc8',
   state: {
     orgLogin: 'wiki',
     repoName: 'General collection',
