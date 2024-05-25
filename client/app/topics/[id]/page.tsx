6e939b5a3c2fad77137f0a0614df2e06ae4eeae2
Eric Walker <eric.walker@gmail.com>
2024-05-25 11:43:06 -0600

    Add support for visiting specific topics

---
+++ client/app/topics/[id]/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { Loader } from '@mantine/core'
+import TopicPage from '@/components/TopicPage'
+
+export const dynamic = 'force-dynamic'
+
+type Props = {
+  params: {
+    id: string,
+  },
+}
+
+export default function GET({ params }: Props) {
+  if (params.id == null) return <Loader color="blue" />
+
+  return <TopicPage topicId={params.id} />
+}
